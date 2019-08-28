c7939480974066c6d04726250fe49ecb12e4cfa6
黎小龙 <228699456@qq.com>
2019-08-28 09:11:29 +0800

    Initial Commit

---
+++ SilenceProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SilenceProject
+//
+//  Created by 甘延娇 on 2019/8/28.
+//  Copyright © 2019 黎小龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
